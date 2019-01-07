e03991458cc684eddb5d848b256dcef0cb223180
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-07 18:01:55 -0500

    Created an associative array of key-value pairs for all the cupcake flavors and used that array to display all flavor options

--- index.php
+++ index.php
@@ -8,6 +8,19 @@
 
     <h1>Order Cupcakes</h1>
 
+    <?php
+    //Initialize variables
+    $fname = "";
+    $lname = "";
+    $custFlavors = "";
+
+    //If form is submitted, process the data
+    if (!empty($_POST)) {
+        require('cupcake-confirmation.php');
+    }
+
+    ?>
+
     <form method="post" action="">
 
         <!-- Customer name -->
@@ -15,18 +28,37 @@
             <legend>Contact Info</legend>
             <label>First Name:&nbsp;
                 <input type="text" size="20" maxlength="20"
-                       name="fname" id="fname" value="">
+                       name="fname" id="fname" value="<?php echo $fname; ?>">
             </label><br>
+            <br>
             <label>Last Name:&nbsp;
                 <input type="text" size="20" maxlength="20"
-                       name="lname" id="lname" value="">
+                       name="lname" id="lname" value="<?php echo $lname; ?>">
             </label><br><br>
         </fieldset>
+        <br>
 
         <!-- Preferred flavor(s) -->
         <fieldset>
             <legend>Pick One or More Flavors</legend>
-            <label>
+
+            <?php
+
+                $flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple Bacon",
+                    "carrot" => "Carrot Walnut", "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet",
+                    "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
+
+                foreach ($flavors as $abbreviation => $flavorName) {
+
+                    echo "<label><input type=\"checkbox\" value=\"$abbreviation\" name=\"flavors[]\"> $flavorName</label><br>";
+
+                }
+
+
+
+
+            ?>
+            <!-- <label>
                 <input type="checkbox" value="grasshopper" name="flavors[]">&nbsp;The Grasshopper
             </label><br>
             <label>
@@ -46,8 +78,9 @@
             </label><br>
             <label>
                 <input type="checkbox" value="tiramisu" name="flavors[]"> Tiramisu
-            </label><br>
+            </label><br> -->
         </fieldset>
+        <br>
 
         <!-- Submit order -->
         <input type="submit" value="Order">
