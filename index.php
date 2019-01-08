beba91e6ed9a17c4d8d003016d31c9c9d36a55a8
Sam Gabriel <sgabriel2@mail.greenriver.edu>
2019-01-07 19:58:06 -0500

    Fixed code formatting and added line comments and a comment header with my name, date, page URL, and page description.

--- index.php
+++ index.php
@@ -1,28 +1,41 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!--
+
+       Author Name: Samantha Gabriel
+       Date: January 7, 2019
+       File Name: index.php
+
+       This is the cupcake fundraiser order form.
+
+    -->
     <meta charset="UTF-8">
     <title>Cupcake Fundraiser</title>
 </head>
 <body>
 
+    <!-- Page header -->
     <h1>Order Cupcakes</h1>
 
     <?php
-    //Initialize variables
-    $fname = "";
-    $lname = "";
-    $custFlavors = "";
 
-    //If form is submitted, process the data
-    if (!empty($_POST)) {
-        require('cupcake-confirmation.php');
-    }
+        // Initialize variables
+        $fname = "";
+        $lname = "";
+        $custFlavors = "";
 
-    echo "<br><br>";
+        // If form is submitted, process the data
+        if (!empty($_POST)) {
+            require('cupcake-confirmation.php');
+        }
+
+        // Adds space between the order confirmation and the form
+        echo "<br><br>";
 
     ?>
 
+    <!-- Order form -->
     <form method="post" action="">
 
         <!-- Customer name -->
@@ -33,6 +46,7 @@
                        name="fname" id="fname" value="<?php echo $fname; ?>">
             </label><br>
             <br>
+
             <label>Last Name:&nbsp;
                 <input type="text" size="20" maxlength="20"
                        name="lname" id="lname" value="<?php echo $lname; ?>">
@@ -46,21 +60,23 @@
 
             <?php
 
+                // Associate array of flavors
                 $flavors = array("grasshopper" => "The Grasshopper", "maple" => "Whiskey Maple Bacon",
                     "carrot" => "Carrot Walnut", "caramel" => "Salted Caramel Cupcake", "velvet" => "Red Velvet",
                     "lemon" => "Lemon Drop", "tiramisu" => "Tiramisu");
 
+                // Iterates through the array and displays all the options in the form
                 foreach ($flavors as $abbreviation => $flavorName) {
 
                     echo "<label><input type=\"checkbox\" value=\"$abbreviation\" name=\"flavors[]\" ";
 
+                    // Makes this section "sticky"
                     echo (isset($_POST['flavors']) && in_array("$abbreviation", $_POST['flavors'])) ? "checked = 'checked'" : '';
 
                     echo "> $flavorName</label><br>";
                 }
 
             ?>
-
         </fieldset>
         <br>
 
